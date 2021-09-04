7e438a5f9e70c9ab8544f3cbf3ffbed8da9163ee
jessicasbatista <jes.santosb@gmail.com>
2021-09-04 19:07:47 -0300

    Criei o teste para incluir o pet

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,39 @@
+package petstore;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class Pet {
+
+    String uri = "https://petstore.swagger.io/v2/pet";
+
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    //Incluir - Create - Post
+    @Test
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        // Sintaxe Gherkin
+        // Dado - Quando - Então
+        // Given - When - Then
+
+        given() // Dado
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when() // Quando
+                .post(uri)
+        .then() // Então
+                .log().all()
+                .statusCode(200)
+        ;
+    }
+}
